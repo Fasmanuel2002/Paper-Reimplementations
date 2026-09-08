fa18e6cff16d2da9801309560ea52a3c887eb8d6
ManuelFas <manuelfasruiz@gmail.com>
2026-09-08 12:07:10 +0200

    Finished the ViT model

--- 04_An_Image_Is_Worth_16x16_Words/model/ViT.py
+++ 04_An_Image_Is_Worth_16x16_Words/model/ViT.py
@@ -1,11 +1,91 @@
+from typing import Optional
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+class VisionTransformer(nn.Module):
+    def __init__(self, n_heads: int, d_dimensionality: int, mlp_size: int,
+                 n_layers: int, image_size: int, patch_size: int, in_channels: int, 
+                 num_classes: int, dropout: float, device: str) -> None:
+        super().__init__()
+        
+        self.device = device
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.in_channels = in_channels
+        self.patch_embedding = PatchEmbedding(d_dimensionality=d_dimensionality, image_size=image_size, patch_size=patch_size, in_channels=in_channels, dropout=dropout)
+        self.encoder_blocks = nn.ModuleList([Block(n_heads=n_heads,
+                                      d_dimensionality=d_dimensionality,
+                                      mlp_size=mlp_size,
+                                      dropout=dropout) for _ in range(n_layers)])
+        
+        self.layer_normalization_final = nn.LayerNorm(d_dimensionality)
+        self.final_head_mlp = nn.Linear(d_dimensionality, num_classes)
+        
+        self.apply(self._init_weights)
+        
+        
+    def _init_weights(self, module : nn.Module):
+        if isinstance(module, nn.Linear):
+            nn.init.normal_(module.weight, mean=0.0, std=0.2)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias) #Make the biases zero
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02) #Gaussian initialization of the weights
+            
+            
+    def forward(self, inputs, targets : Optional[torch.Tensor] = None):
+        x = self.patch_embedding(inputs)
+        
+        all_attention_weights = []
+        for block in self.encoder_blocks:
+            x, attention_weights = block(x)
+            all_attention_weights.append(attention_weights)
+        
+        x = self.layer_normalization_final(x)
+        cls_token_final = x[:, 0] # Shape: (B, d_dimensionality)
+        logits = self.final_head_mlp(cls_token_final) # Shape: (B, num_classes)
+        
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits, targets)
+        #Stack all the attention for make the matrix
+        attention_stack = torch.stack(all_attention_weights, dim=1)
+        
+        return logits, loss, attention_stack
+            
+        
+        
+class Block(nn.Module):
+    def __init__(self, n_heads : int, d_dimensionality : int, mlp_size : int, dropout : float) -> None:
+        super().__init__()
+        head_size = d_dimensionality // n_heads
+        assert d_dimensionality % n_heads == 0, f"The dimensionality of the images: {d_dimensionality}, must be divisionable for the number of heads {n_heads}"
+        
+        self.multi_head_attention = MultiHeadAttention(
+            n_heads=n_heads,
+            d_dimensionality=d_dimensionality,
+            head_size=head_size, 
+            dropout=dropout)
+        
+        self.fnn = MLP(d_dimensionality=d_dimensionality, mlp_size=mlp_size, dropout=dropout)
+        self.ln1 = nn.LayerNorm(d_dimensionality)
+        self.ln2 = nn.LayerNorm(d_dimensionality)
+        
 
-
-
+    def forward(self, x):
+        residual_connection_1 = x
+        x, attention_weigths = self.multi_head_attention(self.ln1(x))
+        x = x + residual_connection_1 # MHA + first residual connection
+        
+        residual_connection_2 = x
+        x = self.fnn(self.ln2(x))
+        x = x + residual_connection_2 #MLP + second residual connection
+        
+        return x, attention_weigths
+    
 class MLP(nn.Module):
     def __init__(self, d_dimensionality : int, mlp_size : int, dropout : float) -> None:
         super().__init__()
