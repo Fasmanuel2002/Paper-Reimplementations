7a7b681321b9d1af16e5d8049504bef65865094f
ManuelFas <manuelfasruiz@gmail.com>
2026-09-11 06:12:55 +0200

    added some hyperparameters from training and changed the name

--- 04_An_Image_Is_Worth_16x16_Words/pre_train.py
+++ 04_An_Image_Is_Worth_16x16_Words/pre_train.py
@@ -1,5 +1,6 @@
 
 import torch
+import torchvision
 from utils.EarlyStopping import EarlyStopping
 from data_preprocessing.vision_dataset import imagenette2Dataset
 from model.ViT import VisionTransformer
@@ -9,16 +10,16 @@ from torch.utils.tensorboard import SummaryWriter # type: ignore
 
 def main():
     # Hyperparameters
-    n_layers = 6
-    d_dimensionality = 256
-    mlp_size = 768
-    n_heads = 4
+    n_layers = 12             
+    d_dimensionality = 384    
+    mlp_size = 1536           
+    n_heads = 6
     dropout = 0.1
     patch_size = 16
     input_channels = 3
     image_size = 224
     n_classes = 10
-    batch_size = 32 
+    batch_size = 64
     patience = 5
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
@@ -51,10 +52,12 @@ def main():
                                        num_classes=n_classes,
                                        device=device).to(device)
     
-    optimizer = torch.optim.AdamW(vision_transformer.parameters(), lr=3e-3, weight_decay=0.3)
+    optimizer = torch.optim.AdamW(vision_transformer.parameters(), lr=3e-4, weight_decay=0.05)
+
+
     lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, 
-        T_max=50, 
+        T_max=100, 
         eta_min=1e-6
     )
     
@@ -80,6 +83,13 @@ def main():
                     "Val": val_loss
                     }, epoch)
         
+        tensor_board_writer.add_scalar("Accuracy/Val", val_acc, epoch)
+        # Grab a single batch from the validation loader
+        images, labels = next(iter(validation_loader))
+        
+        img_grid = torchvision.utils.make_grid(images[:16], normalize=True)
+        tensor_board_writer.add_image("Sample_Validation_Images", img_grid, epoch)
+                
         if early_stopping.early_stop:
             print("Finished because of the early Stopping")
             break 
